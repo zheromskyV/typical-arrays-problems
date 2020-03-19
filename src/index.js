47d800f7f4854c689f592bf4387749544e0327a1
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-03-19 16:39:25 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+	if ((!Array.isArray(array)) || (!array.length)) {
+		return 0;
+	}
+	array.sort((a, b) => a - b);
+	return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+	if ((!Array.isArray(array)) || (!array.length)) {
+		return 0;
+	}
+	array.sort((a, b) => b - a);
+	return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if ((!Array.isArray(array)) || (!array.length)) {
+		return 0;
+	}
+	let sum = 0;
+	array.forEach(a => {
+		sum += a;
+	});
+	return sum / array.length;
 }
